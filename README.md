72c90d1d65c22295dacd27e9182776978b4af2b3
JonyEpsilon <github@j-star.org>
2014-02-19 16:39:40 +0000

    Prepare for 0.1.0.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # lein-gorilla
 
-A Leiningen plugin for the Gorilla REPL.
+A Leiningen plugin for the Gorilla REPL. See the [Gorilla REPL repository](https://github.com/JonyEpsilon/gorilla-repl)
+for more info.
+
+## Licence
+
+The lein Gorilla plugin is licensed to you under the MIT licence. See LICENCE.txt for details.
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-gorilla "0.1.0-SNAPSHOT"
+(defproject lein-gorilla "0.1.0"
   :description "A Leiningen plugin for the Gorilla REPL."
   :url "https://github.com/JonyEpsilon/lein-gorilla"
   :license {:name "MIT"}
-  :dependencies [[gorilla-repl "0.1.0-SNAPSHOT"]]
+  :dependencies [[gorilla-repl "0.1.0"]]
   :eval-in-leiningen true)
