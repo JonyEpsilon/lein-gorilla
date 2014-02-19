81283c5accdbf7b0dd17717014fb6c00f49dbb8e
JonyEpsilon <github@j-star.org>
2014-02-19 19:41:37 +0000

    Bump to 0.1.1 after clojars problems.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-gorilla "0.1.0"
+(defproject lein-gorilla "0.1.1"
   :description "A Leiningen plugin for the Gorilla REPL."
   :url "https://github.com/JonyEpsilon/lein-gorilla"
   :license {:name "MIT"}
-  :dependencies [[gorilla-repl "0.1.0"]]
+  :dependencies [[gorilla-repl "0.1.1"]]
   :eval-in-leiningen true)
