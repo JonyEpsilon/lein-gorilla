a3e7a25447605aed556be47fe0fb4b52873e93f4
JonyEpsilon <github@j-star.org>
2014-02-26 18:57:53 +0000

    Fix for 0.1.2 release.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-gorilla "0.1.1"
+(defproject lein-gorilla "0.1.2"
   :description "A Leiningen plugin for the Gorilla REPL."
   :url "https://github.com/JonyEpsilon/lein-gorilla"
   :license {:name "MIT"}
-  :dependencies [[gorilla-repl "0.1.1"]]
+  :dependencies [[gorilla-repl "0.1.2"]]
   :eval-in-leiningen true)
--- src/leiningen/gorilla.clj
+++ src/leiningen/gorilla.clj
@@ -10,14 +10,13 @@
             [clojure.pprint :as pp]))
 
 ;; This is the leiningen task. It needs no arguments, and can run outside a project (assuming you've got the plugin
-;; installed in your profile). You can pass the arguments:
-;; - :worksheet path/to/worksheet  --  loads the indicated worksheet in a new gorilla instance
+;; installed in your profile).
 (defn ^:no-project-needed gorilla
   [project & opts]
   (let [opts-map (apply hash-map opts)
         port (read-string (or (get opts-map ":port") "8990"))
         curr-deps (or (:dependencies project) [])
-        new-deps (conj curr-deps '[gorilla-repl/gorilla-repl "0.1.0-SNAPSHOT"])
+        new-deps (conj curr-deps '[gorilla-repl/gorilla-repl "0.1.2"])
         prj (assoc project :dependencies new-deps)]
     (when-let [w (get opts-map ":worksheet")] ())
     (eval/eval-in-project
