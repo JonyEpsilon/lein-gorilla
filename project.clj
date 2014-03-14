a9e2fc7f8f5675142f8523c1f38fb710b08eb79a
JonyEpsilon <github@j-star.org>
2014-03-14 11:11:18 +0000

    Bump version to 0.2.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-gorilla "0.1.3-SNAPSHOT"
+(defproject lein-gorilla "0.2.0-SNAPSHOT"
   :description "A Leiningen plugin for the Gorilla REPL."
   :url "https://github.com/JonyEpsilon/lein-gorilla"
   :license {:name "MIT"}
-  :dependencies [[gorilla-repl "0.1.3-SNAPSHOT"]]
+  :dependencies [[gorilla-repl "0.2.0-SNAPSHOT"]]
   :eval-in-leiningen true)
