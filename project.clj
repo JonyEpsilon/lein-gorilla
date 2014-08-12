4ab3b6e38514fd602d7095424c95b537eef70349
JonyEpsilon <github@j-star.org>
2014-08-12 16:05:33 +0100

    Prepare for 0.3.2 release.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-gorilla "0.3.2-SNAPSHOT"
+(defproject lein-gorilla "0.3.2"
   :description "A Leiningen plugin for the Gorilla REPL."
   :url "https://github.com/JonyEpsilon/lein-gorilla"
   :license {:name "MIT"}
-  :dependencies [[gorilla-repl "0.3.2-SNAPSHOT"]]
+  :dependencies [[gorilla-repl "0.3.2"]]
   :eval-in-leiningen true)
